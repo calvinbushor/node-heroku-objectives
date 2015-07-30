6b826ef7d7008ba0fd5e671fef0713eec006c9eb
unknown <anandikonda1@4T871R1.mi.corp.rockfin.com>
2015-07-30 10:17:23 -0400

    Added user name

--- Objective 2 - Chat App/AnuChat/app.js
+++ Objective 2 - Chat App/AnuChat/app.js
@@ -1,17 +1,43 @@
 var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
+	io = require('socket.io').listen(server),
+	nicknames = [];
 	
-server.listen(3030);
-app.use(express.static(__dirname + '/public'));
+server.listen(3000);
 
 app.get('/', function(req, res){
 	res.sendfile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function(socket){
+	
+	socket.on('new user', function(data, callback){
+		if (nicknames.indexOf(data) != -1){
+			callback(false);
+		} else{
+			callback(true);
+			socket.nickname = data;
+			nicknames.push(socket.nickname);
+			updateNicknames();
+			console.log('a user connected');
+		}
+	});
+	
+	
+	
+	function updateNicknames(){
+		io.sockets.emit('usernames', nicknames);
+	}
+
 	socket.on('send message', function(data){
-		io.sockets.emit('new message', data);
+		io.sockets.emit('new message', {msg: data, nick: socket.nickname});
+	});
+	
+	socket.on('disconnect', function(data){
+		if(!socket.nickname) return;
+		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+		updateNicknames();
+		console.log('a user disconnected');
 	});
 });
