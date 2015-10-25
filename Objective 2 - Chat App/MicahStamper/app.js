71a7cb484744176c150a3e504bca2cbc146de453
mstamper01 <mstamper01@gmail.com>
2015-10-25 15:03:29 -0400

    condensed message to a type to setup for chat log

--- Objective 2 - Chat App/MicahStamper/app.js
+++ Objective 2 - Chat App/MicahStamper/app.js
@@ -8,7 +8,6 @@ app.get('/', function(req, res){
 });
 
 var allClients = {};
-var chatLog = {};
 
 //this holds all functions for the chat room
 io.on('connection', function(socket){
@@ -16,30 +15,29 @@ io.on('connection', function(socket){
 	//this part is when someone joins, it stores the socket and username
 	//for tracking purposes
 	socket.on('joined', function(username){
-		io.emit('joined', username + " joined the room...");
-		allClients[socket.id] = [];
+		allClients[socket.id] = {socket: socket, name:username};
 		allClients[socket.id].socket = socket;
-		allClients[socket.id].username = username;
+		allClients[socket.id].name = username;
+		io.emit('chat message', {name:username, msg: username + " has joined the room", type:"join"});
 	});
 
-	//this will transmit the current conversation
-/*
-	}*/
-	//this function emits a message when sent
+	//this function emits a message when sent and logs
 	socket.on('chat message', function(message){
-		 io.emit('chat message', message);
+		io.emit('chat message', message);
 	});
 
+/*
 	socket.on('typing', function(){
 		var typer = allClients.indexOf(socket);
-		io.emit('typing', allClients[1]["name"] + " is typing...");
+		//TODO: Need to work on this functionality
+		//io.emit('typing', allClients[1]["name"] + " is typing...");
 	});
-
+*/
 	//this function catches a disconnect and matches it to the username
 	//it then emits a message that the user disconnected
 	socket.on('disconnect', function(){
-		var username = allClients[socket.id].username;
-		io.emit('disconnect', username + " left the room...");
+		var username = allClients[socket.id].name;
+		io.emit('chat message', {name:username, msg:username + " left the room...", type:"leave"});
 		delete allClients[socket.id];
 	});
 
